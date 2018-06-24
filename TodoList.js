7777cde2bea6b741a8b8ee5f2828b11cff9138d7
h-hiroki <gyouzakuuka@gmail.com>
2018-06-24 20:54:37 +0900

    作った

--- TodoList.js
+++ TodoList.js
@@ -4,6 +4,7 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -11,8 +12,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#F9F8F8'
   },
   todoContainer: {
+    flexDirection: 'row',
     backgroundColor: '#FFF',
     padding: 10,
+    justifyContent: 'space-between',
   },
 })
 
@@ -22,6 +25,9 @@ export default (props) => (
     props.todos.map((todo, index) => (
       <View key={index} style={styles.todoContainer}>
         <Text>{todo}</Text>
+        <TouchableOpacity　onPress={() => props.onPressDelete(index)}>
+          <Text>DELETE</Text>
+        </TouchableOpacity>
       </View>
     ))
   }
